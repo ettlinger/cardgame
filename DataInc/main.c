d74050032a1c7bd6cc78b3e7c3ab421f5c9a7b61
marc <marc@MarcMac.local>
2016-01-28 14:05:34 -0800

    Initial Commit

---
+++ DataInc/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  DataInc
+//
+//  Created by marc on 1/28/16.
+//  Copyright (c) 2016 marc. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
